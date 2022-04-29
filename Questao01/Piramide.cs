d859e68ec47f6a760df3e16f06ebd578b07300cd
SrMorpheus <brunobarroscc@gmail.com>
2022-04-29 16:59:43 -0300

    Questão 1 resolvida

---
+++ Questao01/Piramide.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questao01
+{
+    public  class Piramide
+    {
+
+        public int N { get; set; }
+
+
+
+        public Piramide(int n)
+        {
+
+            if (n < 1)
+            {
+
+ 
+                Console.WriteLine("O valor de N não pode ser menor que zero");
+
+            } 
+            else
+            {
+
+                N = n;
+            }
+
+
+           
+        }
+
+
+        public void Desenha()
+        {
+
+            for (int i = 1; i <= N; i++)
+            {
+
+                for (int j = 1; j <= N - i; j++)
+                {
+                    Console.Write(" ");
+
+
+                }
+
+                for (int contador = 1; contador <= i; contador++)
+                {
+                    Console.Write(contador);
+
+
+                }
+              
+                for (int contador = i - 1; contador >= 1; contador--)
+                {
+
+                    Console.Write(contador);
+
+                }
+
+                Console.WriteLine();
+
+
+
+            }
+
+
+
+
+        }
+
+    }
+}
+
+
+
---
+++ Questao01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Questao01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Exemplo colocando o valor de N = 9
+            Piramide piramide = new Piramide(10);
+
+            //desenhando a piramide
+            piramide.Desenha();
+
+            //Exemplo colocando valor de N = 0
+            Piramide piramide20 = new Piramide(0);
+
+            
+
+
+
+
+        }
+    }
+}
